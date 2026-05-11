e707dfc809907ed67e5ee9f624e8dc73827654f4
nerdev1 <youssefsaleh1120@gmail.com>
2026-05-11 20:52:34 +0300

    Solved the bug of the cmakelists

--- PartC_Stack_Queue/Problem6_Stack_nextGreaterNumber/NextGreaterNumber.h
+++ PartC_Stack_Queue/Problem6_Stack_nextGreaterNumber/NextGreaterNumber.h
@@ -1,44 +1,45 @@
-#pragma once 
-#include<iostream>
-#include<stack>
-#include<vector>
-using namespace std; 
+#pragma once
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
 
-class NextGreaterNumber {
-private : 
-	vector<int>nums; 
-	stack<int>st; 
+class NextGreaterNumber
+{
+private:
+	vector<int> nums;
+	stack<int> st;
 
 public:
-	NextGreaterNumber(vector<int>nums)
+	NextGreaterNumber(vector<int> nums)
 	{
-		this->nums = nums; 
+		this->nums = nums;
 	}
 
-	vector<int>solve()
+	vector<int> solve()
 	{
-		vector<int>result(nums.size(), -1);
-		for (int i = 0; i <   nums.size(); i++)
+		vector<int> result(nums.size(), -1);
+		for (int i = 0; i < nums.size(); i++)
 		{
 			int current = nums.at(i % nums.size());
 
 			while (!st.empty() && nums.at(st.top()) < current)
 			{
-				result.at(st.top()) = current; 
+				result.at(st.top()) = current;
 				st.pop();
 			}
 
 			if (i < nums.size())
-				st.push(i); 
+				st.push(i);
 		}
-		return result; 
+		return result;
 	}
 
-	void printResult(int index , vector<int>result)
+	void printResult(int index, vector<int> result)
 	{
 		if (index == result.size())
 			return;
-		cout << result.at(index) << " " ;
-		printResult(index + 1, result); 
+		cout << result.at(index) << " ";
+		printResult(index + 1, result);
 	}
 };
